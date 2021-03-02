f145e30283d06609988f90798e7d140d946b7764
Steven Farkas <farkas.stev@gmail.com>
2021-03-02 16:49:29 -0500

    Alg lab6

--- Algorithms/lab6/lab6Greedy.py
+++ Algorithms/lab6/lab6Greedy.py
@@ -1,11 +1,27 @@
 targetWeight = int(input())
 lines = int(input())
-items = [[]]
+items = []
 for x in range(lines):
-    items[x] = input()
+    items.append(input().split())
+
+
+items.sort(key=lambda item: int(item[1])/int(item[2]))
 
 def fractionalKnapsack(items, weight):
+    nums[len(items)]
     if (weight == 0):
-        return 0
+        return nums
     else:
-        
+
+        for x in range(len(items)):
+            if items[x[1]] < weight:
+                nums = fractionalKnapsack(items, weight - items[x[1]])
+                return ++nums[x]
+              
+       
+output = fractionalKnapsack(items, targetWeight)
+
+for x in range(len(output)):
+    print(items[x[0]] + '{' + output[x] + '}' + ' ')
+
+

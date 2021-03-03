2739182d0d7370e09ec5eafc008a05301a50b384
Steven Farkas <farkas.stev@gmail.com>
2021-03-02 23:24:37 -0500

    lab6

--- Algorithms/lab6/lab6Greedy.py
+++ Algorithms/lab6/lab6Greedy.py
@@ -4,24 +4,28 @@ items = []
 for x in range(lines):
     items.append(input().split())
 
-
-items.sort(key=lambda item: int(item[1])/int(item[2]))
-
+sortedItems = items
+sortedItems = sorted(items, key=lambda item: int(item[1])/int(item[2]), reverse=True)
 def fractionalKnapsack(items, weight):
-    nums[len(items)]
-    if (weight == 0):
+    nums = {}
+    if weight == 0:
         return nums
     else:
 
-        for x in range(len(items)):
-            if items[x[1]] < weight:
-                nums = fractionalKnapsack(items, weight - items[x[1]])
-                return ++nums[x]
-              
-       
-output = fractionalKnapsack(items, targetWeight)
+        for x in range(lines):
+            if int(items[x][2]) > 0:
+                if int(items[x][2]) <= weight:
+                    weightLoss = int(items[x][2])
+                    items[x][2] = 0
+                    nums = fractionalKnapsack(items, weight - weightLoss)
+                    nums[items[x][0]] = weightLoss 
+                else:
+                    nums[items[x][0]] = int(items[x][2]) - weight
+                return nums 
+result = fractionalKnapsack(sortedItems, targetWeight)
+for x in range(len(items)):
+    print(items[x][0] + '(' + str(result[items[x][0]]) + ')', end=' ')
 
-for x in range(len(output)):
-    print(items[x[0]] + '{' + output[x] + '}' + ' ')
+
 
 
